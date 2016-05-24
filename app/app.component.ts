19da5d0bd75a7c09cfb7928ca74d26e93473390e
Jhon Mike <developer@jhonmike.com.br>
2016-05-23 23:15:20 -0300

    add angular material 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Dashboard NG2</h1>'
+    template: `<md-toolbar color="primary">
+            Angular Material 2 App
+            </md-toolbar>
+    
+            <div style="padding: 7px">
+                <button md-button>Basic Button</button>
+        <button md-raised-button>Raised Button</button>
+    </div>`,
+    directives: [MdToolbar, MdButton]
 })
 export class AppComponent { }
--- systemjs.config.js
+++ systemjs.config.js
@@ -7,6 +7,7 @@
     var map = {
         'app':                        'app', // 'dist',
         '@angular':                   'node_modules/@angular',
+        '@angular2-material':         'node_modules/@angular2-material',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs':                       'node_modules/rxjs'
     };
@@ -14,7 +15,7 @@
     var packages = {
         'app':                        { main: 'main.js',  defaultExtension: 'js' },
         'rxjs':                       { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { defaultExtension: 'js' }
     };
     var ngPackageNames = [
         'common',
@@ -31,9 +32,28 @@
     ngPackageNames.forEach(function(pkgName) {
         packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
     });
+
+    var materialPackages=[
+        '@angular2-material/core',
+        '@angular2-material/button',
+        '@angular2-material/card',
+        '@angular2-material/checkbox',
+        '@angular2-material/icon',
+        '@angular2-material/input',
+        '@angular2-material/list',
+        '@angular2-material/progress-bar',
+        '@angular2-material/progress-circle',
+        '@angular2-material/radio',
+        '@angular2-material/sidenav',
+        '@angular2-material/toolbar'
+    ];
+    materialPackages.forEach(function(pkgName) {
+        var pkg=pkgName.split('/');
+        packages[pkgName] = { main: pkg[1]+'.js', defaultExtension: 'js' };
+    });
     var config = {
         map: map,
         packages: packages
-    }
+    };
     System.config(config);
 })(this);
