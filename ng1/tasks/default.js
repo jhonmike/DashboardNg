178c186041b13d42e2878f3c97601fd7d5f04e5d
Jhon Mike <developer@jhonmike.com.br>
2016-06-28 00:54:10 -0300

    folder ng1

--- ng1/tasks/default.js
+++ ng1/tasks/default.js
@@ -4,3 +4,7 @@ gulp = require('gulp');
 gulp.task('default', function() {
     gulp.start('libs', 'app', 'css', 'fonts', 'fontuigrid', 'html', 'images', 'connect', 'watch');
 });
+
+gulp.task('dist', function() {
+    gulp.start('libs', 'app', 'css', 'fonts', 'fontuigrid', 'html', 'images');
+});
