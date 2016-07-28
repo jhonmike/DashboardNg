98aa8827795a5feaed259b536d5b6f9c25a5c979
Jhon Mike <developer@jhonmike.com.br>
2016-07-28 00:35:46 -0300

    adding material2

--- ng2/src/system-config.ts
+++ ng2/src/system-config.ts
@@ -7,12 +7,25 @@
 /***********************************************************************************************
  * User Configuration.
  **********************************************************************************************/
-/** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
+  '@angular2-material/core'           : { format: 'cjs', defaultExtension: 'js', main: 'core.js'},
+  '@angular2-material/button'         : { format: 'cjs', defaultExtension: 'js', main: 'button.js'},
+  '@angular2-material/card'           : { format: 'cjs', defaultExtension: 'js', main: 'card.js'},
+  '@angular2-material/checkbox'       : { format: 'cjs', defaultExtension: 'js', main: 'checkbox.js'},
+  '@angular2-material/grid-list'      : { format: 'cjs', defaultExtension: 'js', main: 'grid-list.js'},
+  '@angular2-material/icon'           : { format: 'cjs', defaultExtension: 'js', main: 'icon.js'},
+  '@angular2-material/input'          : { format: 'cjs', defaultExtension: 'js', main: 'input.js'},
+  '@angular2-material/list'           : { format: 'cjs', defaultExtension: 'js', main: 'list.js'},
+  '@angular2-material/progress-bar'   : { format: 'cjs', defaultExtension: 'js', main: 'progress-bar.js'},
+  '@angular2-material/progress-circle': { format: 'cjs', defaultExtension: 'js', main: 'progress-circle.js'},
+  '@angular2-material/radio'          : { format: 'cjs', defaultExtension: 'js', main: 'radio.js'},
+  '@angular2-material/sidenav'        : { format: 'cjs', defaultExtension: 'js', main: 'sidenav.js'},
+  '@angular2-material/toolbar'        : { format: 'cjs', defaultExtension: 'js', main: 'toolbar.js'}
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
