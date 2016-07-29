055348044cf3113568d433f31a748d6e63eb1e27
Jhon Mike <developer@jhonmike.com.br>
2016-07-28 23:18:26 -0300

    adding navbar material

--- ng2/src/app/app.component.ts
+++ ng2/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MdToolbar]
 })
 export class AppComponent {
   title = 'app works!';
