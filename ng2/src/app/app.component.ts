fae35c275add81747fd9f735c9b94974131503a0
Jhon Mike <developer@jhonmike.com.br>
2016-07-28 23:52:44 -0300

    remove menu test

--- ng2/src/app/app.component.ts
+++ ng2/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { MdToolbar } from '@angular2-material/toolbar'
-import { MD_MENU_DIRECTIVES } from '@angular2-material/menu';
 
 @Component({
     moduleId: module.id,
     selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css'],
-    directives: [ROUTER_DIRECTIVES, MdToolbar, MD_MENU_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, MdToolbar]
 })
 export class AppComponent {
     title = 'App NG2';
